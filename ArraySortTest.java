ee35940403624f759722ac8413dcfd98f8121f8c
WMcKibbin <WilliamS.McKibbin@gmail.com>
2016-03-23 15:19:49 -0400

    Completed with tests passing

--- ArraySortTest.java
+++ ArraySortTest.java
@@ -1,75 +1,110 @@
+import static org.junit.Assert.*;
 
-public static int[][] arr;
-public static int[][] beforeArr;
-public static int[] legnths;
-public static final int MAX_SIZE = 300;
-public static final int NUM_ARR = 100;
-@BeforeClass
-public static void beforeAllSetUp(){
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import java.util.*;
+
+
+
+public class ArraySortTest
 {
-  Random rand = new Random();
-  for(int i = 0; i < NUM_ARR; i++)
+
+	public static int[][] arr;
+	public static int[][] beforeArr;
+	public static int[] lengths;
+	public static final int MAX_SIZE = 300;
+	public static final int NUM_ARR = 100;
+
+  @BeforeClass
+  public static void beforeAllSetUp()
   {
-    arr[i] = new int[rand.nextInt(MAX_SIZE)];
-    beforeArr[i] = new int[arr[i].length];
-    lengths[i] = arr[i].length;
-    for (int j = 0; j < arr[i].length; j++)
+    Random rand = new Random();
+		arr = new int[NUM_ARR][];
+		beforeArr = new int[NUM_ARR][];
+		lengths = new int[NUM_ARR];
+    for(int i = 0; i < NUM_ARR; i++)
     {
-        arr[i][j] = rand.nextInt();
-        beforeArr[i][j] = arr[i][j];
+      arr[i] = new int[rand.nextInt(MAX_SIZE)];
+      beforeArr[i] = new int[arr[i].length];
+      lengths[i] = arr[i].length;
+      for (int j = 0; j < arr[i].length; j++)
+      {
+          arr[i][j] = rand.nextInt();
+          beforeArr[i][j] = arr[i][j];
+      }
+      Arrays.sort(arr[i]);
     }
-    Arrays.sort(arr[i]);
+
+  }
+  @Test
+  public void arraysSortedTest()
+  {
+    boolean incorrect = false;
+    int min = 0;
+    for(int i = 0; i < NUM_ARR; i++)
+    {
+      min = arr[i][0];
+      for (int j = 0; j < arr[i].length; j++)
+      {
+        if(arr[i][j] <  min)
+        {
+          min = arr[i][j];
+          incorrect = true;
+        }
+      }
+    }
+    assertFalse(incorrect);
   }
 
-}
-@Test
-public static void arraysSortedTest()
-{
-  int temp;
-  boolean incorrect = false
-  min = Integer.MIN_VALUE;
-  for(int i = 0; i < NUM_ARR; i++)
+  @Test
+  public void arraysLengthTest()
   {
-    min = arr[i][0]
-    for (int j = 0; j < arr[i].length; j++)
+    boolean incorrect = false;
+    for(int i = 0; i < NUM_ARR; i++)
     {
-      if(min < arr[i][j])
+      if(lengths[i] != arr[i].length)
       {
-        min = arr[i][j];
         incorrect = true;
       }
     }
+    assertFalse(incorrect);
   }
-  assertFalse(incorrect);
-}
+  @Test
+  public void arraysElementsTest()
+  {
+    boolean[][] beforeCheck = new boolean[NUM_ARR][];
+    boolean[][] afterCheck = new boolean[NUM_ARR][];
+    boolean incorrect = false;
+    for(int i = 0; i < NUM_ARR; i++)
+    {
+      //init boolean check arrays
+      beforeCheck[i] = new boolean[beforeArr[i].length];
+      afterCheck[i] = new boolean[arr[i].length];
+      Arrays.fill(beforeCheck[i], false);
+      Arrays.fill(afterCheck[i], false);
+      for(int j = 0; j < beforeArr[i].length; j++)
+      {
+          for(int k = 0; k < arr[i].length; k++)
+          {
+            if((arr[i][k] == beforeArr[i][j])&&(afterCheck[i][k] != true))
+            {
+              beforeCheck[i][j] = true;
+              afterCheck[i][k] = true;
+              break;
+            }
+          }
+      }
 
-@Test
-public static void arraysLengthTest
-{
-  boolean incorrect = false;
-  for(int i = 0; i < NUM_ARR; i++)
-  {
-    if(lengths[i] != arr[i].length)
-    {
-      incorrect = true;
+      for(int j = 0; j < beforeCheck[i].length; j++)
+      {
+        if(!beforeCheck[i][j] || !afterCheck[i][j])
+        {
+          incorrect = true;
+					break;
+        }
+      }
     }
-  }
-  assertFalse(incorrect);
-}
-@Test
-public static void arraysElementsTest
-{
-  boolean[][] checkBefore;
-  boolean[][] checkAfter;
-  for(int i = 0; i < NUM_ARR; i++)
-  {
-    checkBefore[i] = new boolean[beforeArr[i].length];
-    checkAfter[i] = new boolean[arr[i].length];
-    Arrays.fill(beforeCheck[i], false);
-    Arrays.fill(afterCheck[i], false);
-    for(int j = 0; j < arr[i].length; j++)
-    {
-      
-    }
+    assertFalse(incorrect);
   }
 }
